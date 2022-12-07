13c89260b4dde2b59828943d121e5742d23baee5
JackyWong <Hzqi_110@hotmail.com>
2022-12-07 11:48:09 +0800

    update script with linux and macox on gpu

--- convert-web-download-video.sh
+++ convert-web-download-video.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
 
+function get-hardward-codec() {
+	if [[ "$OSTYPE" =~ ^darwin ]]; then
+		# Mac
+		echo "h264_videotoolbox"
+	elif [[ "$OSTYPE" =~ ^linux ]]; then
+		# Linux
+		echo "h264_vaapi"
+	else
+		# Windowx
+		echo "h264_amf"
+	fi
+}
+
 function convert-web-download-video() {
 	file1="$1_0._src"
 	file2="$1_1._src"
 	target="$1.mp4"
+
+	codec=`get-hardward-codec`
+
 
     # 解第一个参数音频
 	ffmpeg -i "$file1" -vn -ab 128k -ar 44100 -strict -2 -y "1_audio.aac"
@@ -24,29 +40,27 @@ function convert-web-download-video() {
 			fi
 		else
 			# 解第一个参数视频
-			ffmpeg -i "$file1" -i "2_audio.aac" -c copy -shortest "result.mp4"
+			ffmpeg -i "$file1" -i "2_audio.aac" -c copy -shortest -c:v "$codec" -b:v 8000k "result.mp4"
 			if [ $? -ne 0 ]; then
 				# 视频失败
 				echo -e "\033[31mCOVERT-WEB-DOWNLOAD-VIDER FAIL ON VIDER.\033[0m"
 				rm result.mp4
 			else
-				# 最后转码
-				ffmpeg -i "result.mp4" "$target"
+				# 最后改名
+				mv result.mp4 "$target"
 				rm 2_audio.aac
-				rm result.mp4
 			fi
 		fi
 	else
 		# 解第二个参数视频
-		ffmpeg -i "$file2" -i "1_audio.aac" -c copy -shortest "result.mp4"
+		ffmpeg -i "$file2" -i "1_audio.aac" -c copy -shortest -c:v "$codec" -b:v 8000k "result.mp4"
 		if [ $? -ne 0 ]; then
 			# 视频失败
 			echo -e "\033[31mCOVERT-WEB-DOWNLOAD-VIDER FAIL ON VIDER.\033[0m"
 		else
-			# 最后转码
-			ffmpeg -i "result.mp4" "$target"
+			# 最后改名
+			mv result.mp4 "$target"
 			rm 1_audio.aac
-			rm result.mp4
 		fi
 	fi
 }
